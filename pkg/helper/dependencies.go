854aa72c97654923178cd706bdb5d4d16d93516e
ggtrd <103002419+ggtrd@users.noreply.github.com>
2026-02-16 14:44:42 +0100

    add static dependency

--- pkg/helper/dependencies.go
+++ pkg/helper/dependencies.go
@@ -26,6 +26,7 @@ func DownloadStaticDependencies() {
 	var dependencies = []StaticDependency{
 		StaticDependency{"https://unpkg.com/openpgp@latest/dist/openpgp.min.js", "static/dynamic/openpgp.min.js"},
 		StaticDependency{"https://unpkg.com/slate@latest/dist/index.js", "static/dynamic/slate.js"},
+		StaticDependency{"https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js", "static/dynamic/qrcode.min.js"},
 		StaticDependency{"https://raw.githubusercontent.com/FortAwesome/Font-Awesome/refs/tags/7.1.0/css/all.min.css", "static/dynamic/fonts/css/fontawesome.all.min.css"},
 		StaticDependency{"https://github.com/FortAwesome/Font-Awesome/raw/refs/tags/7.1.0/webfonts/fa-brands-400.woff2", "static/dynamic/fonts/webfonts/fa-brands-400.woff2"},
 		StaticDependency{"https://github.com/FortAwesome/Font-Awesome/raw/refs/tags/7.1.0/webfonts/fa-solid-900.woff2", "static/dynamic/fonts/webfonts/fa-solid-900.woff2"},
